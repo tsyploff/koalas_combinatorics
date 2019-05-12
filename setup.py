511185a8c39cc7721e940f323269014e673ea581
Алексей <43031739+tsyploff@users.noreply.github.com>
2019-05-12 08:59:52 +0300

    Moved "setup.py" to "python_c_api"

--- setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('combinatorics', sources = ['combinatorics.c'])
-
-setup (name = 'combinatorics',
-       version = '1.0',
-       description = 'Модуль для решения комбинаторных задач',
-       ext_modules = [module1])
